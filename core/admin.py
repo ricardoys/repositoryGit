1d02a423a78df46e1f386f2dcc124c47fbfa78e6
ricardoys <rysassagawa@gmail.com>
2025-06-11 22:49:49 -0500

    Migração de models: Repositorio, GitUser, Issue e Commit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Repositorio, GitUser, Issue, Commit
+
+admin.site.register(Repositorio)
+admin.site.register(GitUser)
+admin.site.register(Issue)
+admin.site.register(Commit)
